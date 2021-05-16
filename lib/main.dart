191d8f135204b2c24f19653dd0398b3c75a27656
Phantonym21 <avishkardige2001@gmail.com>
2021-05-17 00:32:12 +0530

    added home page with data table:

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'storage_utils.dart';
 import 'form_input_card.dart';
+import 'package:table_sticky_headers/table_sticky_headers.dart';
 
 void main() {
   runApp(ExpenseApp());
@@ -12,28 +13,186 @@ class ExpenseApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          backgroundColor: Color.fromRGBO(20, 61, 89, 30),
-          title: Center(
-            child: Text(
-              'Add Expense',
-              style: TextStyle(
-                color: Color.fromRGBO(244, 180, 26, 1),
-                fontFamily: 'Hallo sans',
-                fontWeight: FontWeight.bold,
-                fontSize: 25,
-              ),
+      home: HomeRoute(),
+    );
+  }
+}
+
+class HomeRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(20, 61, 89, 30),
+        title: Center(
+          child: Text(
+            'Your Expenses',
+            style: TextStyle(
+              color: Color.fromRGBO(244, 180, 26, 1),
+              fontFamily: 'Hallo sans',
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
             ),
           ),
         ),
-        body: SafeArea(
-          child: AddExpensePage(),
+      ),
+      body: SafeArea(
+        child: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color.fromRGBO(244, 180, 26, 1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(244, 180, 26, 1),
+              ),
+            ),
+          ),
+          // Data List
+          Expanded(
+            flex: 10,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(20, 61, 89, 30),
+              ),
+              child: DataTable(
+                headingTextStyle: TextStyle(
+                  color: Color.fromRGBO(244, 180, 26, 1),
+                  fontWeight: FontWeight.bold,
+                ),
+                dataTextStyle: TextStyle(
+                  color: Color.fromRGBO(244, 180, 26, 1),
+                ),
+                showBottomBorder: true,
+                columns: [
+                  DataColumn(label: Text('Item')),
+                  DataColumn(label: Text('Cost')),
+                  DataColumn(label: Text('Date')),
+                ],
+                rows: [
+                  DataRow(cells: [
+                    DataCell(Text('Shoes')),
+                    DataCell(Text('500')),
+                    DataCell(Text('12/05/21')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Clothes')),
+                    DataCell(Text('2000')),
+                    DataCell(Text('13/05/21')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Watch')),
+                    DataCell(Text('100')),
+                    DataCell(Text('12/07/21')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Clothes')),
+                    DataCell(Text('2000')),
+                    DataCell(Text('13/05/21')),
+                  ]),
+                  DataRow(cells: [
+                    DataCell(Text('Clothes')),
+                    DataCell(Text('2000')),
+                    DataCell(Text('13/05/21')),
+                  ]),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: Color.fromRGBO(20, 61, 89, 30),
+                      shape: CircleBorder(),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => AddExpenseRoute()),
+                      );
+                    },
+                    child: Center(
+                      child: Text(
+                        '+',
+                        style: TextStyle(
+                          color: Color.fromRGBO(244, 180, 26, 1),
+                          fontFamily: 'Hallo sans',
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: SizedBox(),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: SizedBox(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AddExpenseRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Color.fromRGBO(20, 61, 89, 30),
+        title: Center(
+          child: Text(
+            'Add Expense',
+            style: TextStyle(
+              color: Color.fromRGBO(244, 180, 26, 1),
+              fontFamily: 'Hallo sans',
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+            ),
+          ),
         ),
-        // bottomNavigationBar: AppBar(
-        //   title: Text('Navigation Bar'),
-        // ),
+      ),
+      body: SafeArea(
+        child: AddExpensePage(),
       ),
     );
   }
@@ -48,35 +207,38 @@ class _AddExpensePageState extends State<AddExpensePage> {
   // double textBoxOpacity = 1;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(244, 180, 26, 1),
+    return Container(
+      color: Color.fromRGBO(244, 180, 26, 1),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(244, 180, 26, 1),
+              ),
             ),
           ),
-        ),
-        Expanded(
-          flex: 6,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(244, 180, 26, 1),
-            ),
-            child: AddExpenseForm(),
-          ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Color.fromRGBO(244, 180, 26, 1),
+          Expanded(
+            flex: 6,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(244, 180, 26, 1),
+              ),
+              child: AddExpenseForm(),
             ),
           ),
-        ),
-      ],
+          Expanded(
+            flex: 1,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(244, 180, 26, 1),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -180,6 +342,8 @@ class _AddExpenseFormState extends State<AddExpenseForm> {
                         );
 
                         print(await listEntries());
+
+                        Navigator.pop(context);
                       }
                     },
                     child: Text(
